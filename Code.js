f2b7e9a9b5dd23ab94b3e4857085b1214cd5f3f1
mratamayo-tsatinc <143669058+mratamayo-tsatinc@users.noreply.github.com>
2025-08-12 22:37:44 +0800

    Added the onOpen function in Code.js

--- Code.js
+++ Code.js
@@ -1,1 +1,11 @@
+function onOpen() {
+  const ui = SpreadsheetApp.getUi();
+  ui.createMenu('Custom Functions')
+    .addItem('Fill SNO and Enrollment Status', 'fillSNOAndEnrollmentStatus')
+    .addToUi();
 
+  ui.createMenu("📁 Drive Image Validator")
+    .addItem("📌 Scan Answer Key", "scanAnswerKey")
+    .addItem("📋 Compare Student Folders", "compareStudentFolders")
+    .addToUi();
+}
