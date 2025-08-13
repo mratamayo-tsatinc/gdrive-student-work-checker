25f8310fa6484bb517814a0e90236556c58d4720
mratamayo-tsatinc <markrobert.tamayo@tsatinc.edu.ph>
2025-08-13 08:39:33 +0800

    Remove menu item

--- Code.js
+++ Code.js
@@ -1,9 +1,6 @@
 function onOpen() {
   const ui = SpreadsheetApp.getUi();
-  ui.createMenu('Custom Functions')
-    .addItem('Fill SNO and Enrollment Status', 'fillSNOAndEnrollmentStatus')
-    .addToUi();
-
+  
   ui.createMenu("Drive Image Validator")
     .addItem("Scan Answer Key", "scanAnswerKey")
     .addItem("Compare Student Folders", "compareStudentFolders")
